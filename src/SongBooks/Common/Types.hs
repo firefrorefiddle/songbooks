c855ba0853cbd2219d6795e473dc9258c7db5508
Michael Hartl <mikehartl17@gmail.com>
2016-09-23 06:33:34 +0200

    get rid of some futureback leftovers

--- src/SongBooks/Common/Types.hs
+++ src/SongBooks/Common/Types.hs
@@ -3,21 +3,10 @@
 module SongBooks.Common.Types (
        module System.Posix.Types,
        module Control.Monad.Trans.Except,
-       FileInfo,
-       FileDate,
-       FileMeta(..),
-       Backup(..),
-       FileType(..),
        ErrorMsg(..),
-       BackupCondition(..),
-       BackupRestart(..),
-       RestartCondition(..),
-       RestartHandler,
        FBT,
        FBIO,
        runFBT,
-       UniqueId,
-       BackupId,
        -- re-exports from directory-bytestring
        RawFilePath,
        FileStatus
@@ -35,72 +24,16 @@ import qualified Data.Text as T
 type Id  = Integer
 type Idx = Int
 
-type FileInfo = (RawFilePath, FileStatus)
-
-type FileDate = EpochTime
-
-data FileType = RegularFile | Directory | Symlink | BlockDevice | CharDevice | UnknownFileType
-  deriving (Read, Show, Eq)
-
-type UniqueId = T.Text
-type BackupId = UniqueId
-
--- | All known restarts which may become available during a backup or restore operation.
-data BackupRestart
-    = Overwrite
-    | OverwriteAll
-    | Abort
-    | Retry
-    | Merge
-    | MergeAll
-    deriving (Show, Read, Eq)
-
--- | All known conditions which may occur during a backup or restore operation.
-data BackupCondition
-    = FileExists FileType RawFilePath
-    | WrongFileType RawFilePath FileType FileType
-    | WrongFileMode RawFilePath FileMode FileMode
-    deriving (Show, Eq)
-
--- | A concrete condition which occurred, and the available restarts.
-data RestartCondition = RestartCondition [BackupCondition] [BackupRestart]
-    deriving (Show, Eq)
-
--- | A function which selects one of the available restarts. If the handler 
--- returns 'Nothing', then the next handler is tried. If no handler handles
--- the restart, an error is thrown.
-type RestartHandler m = RestartCondition -> m (Maybe BackupRestart)
-
 -- | An error which occurred in the program.
 data ErrorMsg
     = StringError String -- ^ A simple string error.
     | DBError SqlError   -- ^ An error thrown by Sqlite.
     | IOError E.IOError  -- ^ A disk I/O error
-    | BackupError RestartCondition -- ^ A restartable condition
     deriving (Show, Eq)
-
--- | This is most of the file metadata grouped together and stored as a single node. 
-data FileMeta = FileMeta
-    { metaPermissions :: FileMode
-    , metaOwner       :: (UserID, GroupID)
-    , metaFilesize    :: FileOffset
-    , metaModified    :: EpochTime
-    , metaFileType    :: FileType
-    }
-    deriving (Read, Show, Eq)
-
--- | All the metadata belonging to a single backup run, stored together as a single node.
-data Backup = Backup 
-    { backupId          :: BackupId
-    , backupStartTime   :: EpochTime
-    , backupSource      :: RawFilePath
-    , backupDestination :: RawFilePath
-    , mBackupEndTime    :: Maybe EpochTime
-    } deriving (Eq, Show)
 
 -- | Mainly for testing purposes, a monad which throws 'ErrorMsg's.
 newtype FBT m r = FBT { getFBT :: ExceptT ErrorMsg m r }
-     deriving (Applicative, Monad, MonadIO, Functor) -- MonadError ErrorMsg, Functor)
+     deriving (Applicative, Monad, MonadIO, Functor)
 
 type FBIO = FBT IO
 
